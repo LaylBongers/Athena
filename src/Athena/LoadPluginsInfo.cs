165f087a7abd6a166e2db4fdc55f082a3b72bee3
Layl Conway <laylconway@live.com>
2015-03-20 19:34:31 +0100

    Add support for loading DLLs from folders.

--- src/Athena/LoadPluginsInfo.cs
+++ src/Athena/LoadPluginsInfo.cs
@@ -1,16 +1,37 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace Athena
 {
 	public class LoadPluginsInfo
 	{
+		public Collection<AssemblyName> PluginAssemblies { get; } = new Collection<AssemblyName>();
+
 		public static LoadPluginsInfo FromDirectory(string directory)
 		{
-			throw new NotImplementedException();
-		}
+			var info = new LoadPluginsInfo();
 
-		public Collection<AssemblyName> PluginAssemblies { get; } = new Collection<AssemblyName>();
+			// Verify the directory exists
+			var directoryInfo = new DirectoryInfo(directory);
+			if (!directoryInfo.Exists)
+				throw new InvalidOperationException("Directory \"" + directory + "\" does not exist.");
+			
+			// Find all files ending in .dll, those are plugins
+			var pluginFiles = directoryInfo.GetFiles("*.dll");
+
+			// Create assembly names for those files so they're easier to load in later on
+			var assemblyNames = pluginFiles.Select(f => AssemblyName.GetAssemblyName(f.FullName));
+
+			// Add those assembly names to the collection
+			foreach (var name in assemblyNames)
+			{
+				info.PluginAssemblies.Add(name);
+			}
+
+			return info;
+		}
 	}
 }
